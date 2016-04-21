ad5be31ca34dc7ec947acdb33501b98cb3d1cdb5
eirslett <eirik.sletteberg@finn.no>
2016-04-21 13:23:41 +0200

    Add upload script
    It's a hassle to manually upload new builds
    of the plugin via Google's web UI.
    This will let you upload a newer version of
    the chrome extension programmatically:
    
    node build.js
    node upload.js
    
    For this to work, you need the client secret,
    and a refresh token. Documentation for that will
    come later.

---
+++ upload.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const GoogleAuth = require('google-auth-library');
+const fetch = require('node-fetch');
+
+const appId = 'jdpmaacocdhbmkppghmgnjmfikeeldfe';
+const data = fs.createReadStream('dist/zipkin-chrome-extension.zip');
+
+const clientId = '1077732158179-kdth2msv27g08oair1pvo3apu3ievrsm.apps.googleusercontent.com';
+const clientSecret = fs.readFileSync('client-secret.txt', 'utf-8').trim();
+const refreshToken = fs.readFileSync('refresh-token.txt', 'utf-8').trim();
+
+const auth = new GoogleAuth();
+const OAuth2 = auth.OAuth2;
+const oauth2Client = new OAuth2(clientId, clientSecret, 'urn:ietf:wg:oauth:2.0:oob');
+const webstoreScope = 'https://www.googleapis.com/auth/chromewebstore';
+const scopes = [webstoreScope];
+oauth2Client.setCredentials({
+  refresh_token: refreshToken
+});
+oauth2Client.refreshAccessToken((err, tokens) => {
+  if (err) {
+    console.error('oh no!');
+    console.error(err);
+    const url = oauth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: scopes
+    });
+    console.error('get a new token here', url);
+  } else {
+    // https://developer.chrome.com/webstore/using_webstore_api#uploadexisitng
+    fetch('https://www.googleapis.com/upload/chromewebstore/v1.1/items/' + appId, {
+      method: 'PUT',
+      body: data,
+      headers: {
+        'Authorization': 'Bearer ' + tokens.access_token,
+        'x-goog-api-version': '2'
+      }
+    })
+      .then(res => res.text())
+      .then(console.log)
+      .catch(console.error);
+  }
+});
