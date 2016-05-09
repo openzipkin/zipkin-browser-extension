24e438899c2bea210de3fb69416c3a3d8944c1c5
eirslett <eirik.sletteberg@finn.no>
2016-05-09 17:06:39 +0200

    Update the upload/publish script. Bump version

--- upload.js
+++ upload.js
@@ -1,9 +1,14 @@
+// USAGE:
+// node upload.js upload
+// node upload.js publish
+
 const fs = require('fs');
 const GoogleAuth = require('google-auth-library');
 const fetch = require('node-fetch');
 
+const command = process.argv[2];
+
 const appId = 'jdpmaacocdhbmkppghmgnjmfikeeldfe';
-const data = fs.createReadStream('dist/zipkin-chrome-extension.zip');
 
 const clientId = '1077732158179-kdth2msv27g08oair1pvo3apu3ievrsm.apps.googleusercontent.com';
 const clientSecret = fs.readFileSync('client-secret.txt', 'utf-8').trim();
@@ -27,17 +32,32 @@ oauth2Client.refreshAccessToken((err, tokens) => {
     });
     console.error('get a new token here', url);
   } else {
-    // https://developer.chrome.com/webstore/using_webstore_api#uploadexisitng
-    fetch('https://www.googleapis.com/upload/chromewebstore/v1.1/items/' + appId, {
-      method: 'PUT',
-      body: data,
-      headers: {
-        'Authorization': 'Bearer ' + tokens.access_token,
-        'x-goog-api-version': '2'
-      }
-    })
-      .then(res => res.text())
-      .then(console.log)
-      .catch(console.error);
+    if (command === 'upload') {
+      const data = fs.createReadStream('dist/chrome/zipkin-chrome-extension.zip');
+      // https://developer.chrome.com/webstore/using_webstore_api#uploadexisitng
+      fetch('https://www.googleapis.com/upload/chromewebstore/v1.1/items/' + appId, {
+        method: 'PUT',
+        body: data,
+        headers: {
+          'Authorization': 'Bearer ' + tokens.access_token,
+          'x-goog-api-version': '2'
+        }
+      })
+        .then(res => res.text())
+        .then(console.log)
+        .catch(console.error);
+    } else if (command === 'publish') {
+      fetch('https://www.googleapis.com/chromewebstore/v1.1/items/' + appId + '/publish', {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Bearer ' + tokens.access_token,
+          'x-goog-api-version': '2'
+        }
+      }).then(res => res.text())
+        .then(console.log)
+        .catch(console.error);
+    } else {
+      console.log(`Invalid command "${command}".`);
+    }
   }
 });
