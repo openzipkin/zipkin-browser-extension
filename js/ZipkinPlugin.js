b3fa48d728ebcb17555100355ceda1b0b3061b3d
Simen Bekkhus <sbekkhus91@gmail.com>
2017-09-02 13:51:22 +0200

    Handle redirect to `/zipkin`

--- js/ZipkinPlugin.js
+++ js/ZipkinPlugin.js
@@ -43,7 +43,16 @@ export default class ZipkinPlugin {
     this.storage.set('zipkinUrls', this.zipkinUrls.map(z => ({url: z.url})));
   }
 
-  addZipkinUrl(url, saveToStorage = true) {
+  async addZipkinUrl(url, saveToStorage = true) {
+    const fetchUrl = await fetch(url);
+
+    // Never actually download the body
+    await fetchUrl.body.cancel();
+
+    if (fetchUrl.redirected) {
+      url = `${url}/zipkin`
+    }
+
     this.zipkinUrls.push({
       url,
       statusCheck: this.makeZipkinCheckInterval(url)
